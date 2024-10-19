883642728678099e32439e01337c698a008d6a21
dchrysov <j.chrisov@gmail.com>
2024-10-19 23:32:36 +0300

    ft_putnbr_fd.c: case added where n = INTMIN

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: dimitris <dimitris@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 11:45:20 by dchrysov          #+#    #+#             */
-/*   Updated: 2024/10/19 22:40:53 by dimitris         ###   ########.fr       */
+/*   Updated: 2024/10/19 23:23:18 by dimitris         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,21 @@ int	ft_putnbr_fd(int n, int fd)
 	int	count;
 
 	count = 0;
-	if (n < 0)
+	if (n == INT_MIN)
 	{
-		count += ft_putchar_fd('-', fd);
-		n *= -1;
+		write(fd, "-2147483648", 11);
+		count = 11;
 	}
-	if (n >= 10)
-		count += ft_putnbr_fd(n / 10, fd);
-	count += ft_putchar_fd((n % 10) + '0', fd);
+	else
+	{
+		if (n < 0)
+		{
+			count += ft_putchar_fd('-', fd);
+			n *= -1;
+		}
+		if (n >= 10)
+			count += ft_putnbr_fd(n / 10, fd);
+		count += ft_putchar_fd((n % 10) + '0', fd);
+	}
 	return (count);
 }
